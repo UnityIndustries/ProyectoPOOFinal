69f4cd8fbb3c42aab923e21d0541ae0a3d94bfbf
John Manuel <John Manuel@LAPTOP-4OBTID9R>
2023-07-31 15:40:53 -0400

    hice algunas cambios, elimine errores, etc

--- Proyecto Final POO Grupo 4/src/logico/Componente.java
+++ Proyecto Final POO Grupo 4/src/logico/Componente.java
@@ -1,18 +1,21 @@
 package logico;
 
-public class Componente {
-	
-	protected String marca; 
-	protected String modelo; 
-	protected double precio; 
-	protected int cantidadDisponible;
+public abstract class Componente {
 
-	public Componente(String marca, String modelo, double precio, int cantidadDisponible){
-		this.marca=marca; 
-		this.modelo=modelo; 
-		this.precio=precio; 
-		this.cantidadDisponible=cantidadDisponible; 
+	protected String marca; 
+	protected double precio; 
+	protected int cantidadDisponible; 
+	protected String NumeroSerie; 
+
+	public Componente(String marca, double precio, int cantidadDisponible, String NumeroSerie){
+		this.marca=marca;
+		this.precio=precio;
+		this.cantidadDisponible=cantidadDisponible;
+		this.NumeroSerie=NumeroSerie;   
 	}
+
+	
+	public double getPrecio() { return precio; }
 
 	public String getMarca() {
 		return marca;
@@ -22,27 +25,24 @@ public class Componente {
 		this.marca = marca;
 	}
 
-	public String getModelo() {
-		return modelo;
-	}
-
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
-
-	public double getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
-
 	public int getCantidadDisponible() {
 		return cantidadDisponible;
 	}
 
 	public void setCantidadDisponible(int cantidadDisponible) {
 		this.cantidadDisponible = cantidadDisponible;
-	}		
+	}
+
+	public String getNumeroSerie() {
+		return NumeroSerie;
+	}
+
+	public void setNumeroSerie(String numeroSerie) {
+		NumeroSerie = numeroSerie;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
 }
+
