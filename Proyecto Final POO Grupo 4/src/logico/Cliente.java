403284a7b925977ce738044942b1c5afda5190a3
John Manuel <John Manuel@LAPTOP-4OBTID9R>
2023-07-31 15:32:21 -0400

    hice unos cambios

--- Proyecto Final POO Grupo 4/src/logico/Cliente.java
+++ Proyecto Final POO Grupo 4/src/logico/Cliente.java
@@ -1,22 +1,19 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Cliente {
-<<<<<<< HEAD
-	private String nombre;
-	private String direccion;
-	private String telefono;
-=======
-	
+	private String nombre; 
+	private String telefono; 
+	private String cedula; 
+	private ArrayList<Factura> listaFacturas;
 
-	
->>>>>>> branch 'main' of https://github.com/UnityIndustries/ProyectoPOOFinal.git
-
-	public Cliente(String nombre, String direccion, String telefono) {
+	public Cliente(String nombre, String telefono, String cedula) {
 		this.nombre = nombre;
-		this.direccion = direccion;
 		this.telefono = telefono;
+		this.cedula = cedula;
+		this.listaFacturas = new ArrayList<Factura>();
 	}
-
 
 	public String getNombre() {
 		return nombre;
@@ -26,19 +23,59 @@ public class Cliente {
 		this.nombre = nombre;
 	}
 
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-
 	public String getTelefono() {
 		return telefono;
 	}
 
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public ArrayList<Factura> getListaFacturas() {
+		return listaFacturas;
+	}
+
+	public void setListaFacturas(ArrayList<Factura> listaFacturas) {
+		this.listaFacturas = listaFacturas;
+	}
+
+	
+	public void agregarFactura(Factura factura) {
+		this.listaFacturas.add(factura);
+	}
+
+
+	public void eliminarFactura(Factura factura) {
+		this.listaFacturas.remove(factura);
+	}
+
+
+	public void comprarCombo(Combo combo) {
+		int numeroFactura = 1; 
+		String fechaEmision = "01/01/2022"; 
+		String nombreCliente = this.nombre; 
+
+		Factura factura = new Factura(numeroFactura, fechaEmision, nombreCliente); 
+		factura.agregarCombo(combo); 
+		agregarFactura(factura); 
+	}
+
+	
+	public void comprarComponente(Componente componente) {
+		int numeroFactura = 2;
+		String fechaEmision = "01/01/2022"; 
+		String nombreCliente = this.nombre;
+
+		Factura factura = new Factura(numeroFactura, fechaEmision, nombreCliente); 
+		factura.agregarComponente(componente);
+		agregarFactura(factura);
 	}
 }
