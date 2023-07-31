c433ed0bb369f36423c5b7ceedb9519fe2fb01e2
John Manuel <John Manuel@LAPTOP-4OBTID9R>
2023-07-31 15:49:48 -0400

    codigo arreglado

--- Proyecto Final POO Grupo 4/src/logico/Motherboard.java
+++ Proyecto Final POO Grupo 4/src/logico/Motherboard.java
@@ -1,50 +1,49 @@
 package logico;
 
-public class Motherboard extends Componente1 {
-    private String modelo;
-    private String tipoConectorMicro;
-    private String tipoMemoriaRAM;
-    private String listaConexionesDiscosDuros;
+public class Motherboard extends Componente {
+	private String modelo; 
+	private String tipoConectorSocket; 
+	private String tipoMemoriaRAM; 
+	private int conexionesParaDiscosDuros; 
 
-    public Motherboard(String numeroSerie, String marca, double precio, int cantidadDisponible,
-                       String modelo, String tipoConectorMicro, String tipoMemoriaRAM, String listaConexionesDiscosDuros) {
-        super(numeroSerie, marca, precio, cantidadDisponible);
-        this.modelo = modelo;
-        this.tipoConectorMicro = tipoConectorMicro;
-        this.tipoMemoriaRAM = tipoMemoriaRAM;
-        this.listaConexionesDiscosDuros = listaConexionesDiscosDuros;
-    }
+	public Motherboard(String marca, double precio, int cantidadDisponible, String NumeroSerie, String modelo, String tipoConectorSocket, String tipoMemoriaRAM, int conexionesParaDiscosDuros) {
+		super(marca, precio, cantidadDisponible, NumeroSerie);
+		this.modelo = modelo;
+		this.tipoConectorSocket = tipoConectorSocket;
+		this.tipoMemoriaRAM = tipoMemoriaRAM;
+		this.conexionesParaDiscosDuros = conexionesParaDiscosDuros;
+	}
 
-    
-    public String getModelo() {
-        return modelo;
-    }
+	public String getModelo() {
+		return modelo;
+	}
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
 
-    public String getTipoConectorMicro() {
-        return tipoConectorMicro;
-    }
+	public String getTipoConectorSocket() {
+		return tipoConectorSocket;
+	}
 
-    public void setTipoConectorMicro(String tipoConectorMicro) {
-        this.tipoConectorMicro = tipoConectorMicro;
-    }
+	public void setTipoConectorSocket(String tipoConectorSocket) {
+		this.tipoConectorSocket = tipoConectorSocket;
+	}
 
-    public String getTipoMemoriaRAM() {
-        return tipoMemoriaRAM;
-    }
+	public String getTipoMemoriaRAM() {
+		return tipoMemoriaRAM;
+	}
 
-    public void setTipoMemoriaRAM(String tipoMemoriaRAM) {
-        this.tipoMemoriaRAM = tipoMemoriaRAM;
-    }
+	public void setTipoMemoriaRAM(String tipoMemoriaRAM) {
+		this.tipoMemoriaRAM = tipoMemoriaRAM;
+	}
 
-    public String getListaConexionesDiscosDuros() {
-        return listaConexionesDiscosDuros;
-    }
+	public int getConexionesParaDiscosDuros() {
+		return conexionesParaDiscosDuros;
+	}
 
-    public void setListaConexionesDiscosDuros(String listaConexionesDiscosDuros) {
-        this.listaConexionesDiscosDuros = listaConexionesDiscosDuros;
-    }
+	public void setConexionesParaDiscosDuros(int conexionesParaDiscosDuros) {
+		this.conexionesParaDiscosDuros = conexionesParaDiscosDuros;
+	}
+
 }
