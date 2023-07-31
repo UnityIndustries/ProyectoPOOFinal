d0f35dfba41b8550d03a20e4550755fc2e262cc1
John Manuel <John Manuel@LAPTOP-4OBTID9R>
2023-07-31 15:38:00 -0400

    hice algunos cambios

--- Proyecto Final POO Grupo 4/src/logico/Combo.java
+++ Proyecto Final POO Grupo 4/src/logico/Combo.java
@@ -2,54 +2,15 @@ package logico;
 
 import java.util.ArrayList;
 
-<<<<<<< HEAD
-public class Combo extends Componente1 {
-    private String nombre;
-    private String descripcion;
-    private ArrayList<Componente1> componentesIncluidos;
-
-    public Combo(String nombre, double precio, String descripcion) {
-        super("N/A", "N/A", precio, 0);
-        this.setNombre(nombre);
-        this.setDescripcion(descripcion);
-        this.componentesIncluidos = new ArrayList<>();
-    }
-
-    public void agregarComponenteIncluido(Componente1 componente) {
-        componentesIncluidos.add(componente);
-    }
-
-    public void eliminarComponenteIncluido(Componente1 componente) {
-        componentesIncluidos.remove(componente);
-    }
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-=======
 public class Combo {
-	
-	private String nombre;
-	private String descripcion;
-	private double precio;
-	private ArrayList<Componente> componentes;
+	private String nombre; 
+	private double precioCombo; 
+	private ArrayList<Componente> listaComponentes; 
 
-	public Combo(String nombre, String descripcion, double precio) {
+	public Combo(String nombre, double precioCombo) {
 		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.precio = precio;
-		this.componentes = new ArrayList<>();
+		this.precioCombo = precioCombo;
+		this.listaComponentes = new ArrayList<Componente>();
 	}
 
 	public String getNombre() {
@@ -60,33 +21,26 @@ public class Combo {
 		this.nombre = nombre;
 	}
 
-	public String getDescripcion() {
-		return descripcion;
+	public ArrayList<Componente> getListaComponentes() {
+		return listaComponentes;
 	}
 
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
+	public void setListaComponentes(ArrayList<Componente> listaComponentes) {
+		this.listaComponentes = listaComponentes;
 	}
 
-	public double getPrecio() {
-		return precio;
+	public void setPrecioCombo(double precioCombo) {
+		this.precioCombo = precioCombo;
 	}
 
-	public void setPrecio(double precio) {
-		this.precio = precio;
+	public double getPrecioCombo() { return precioCombo; }
+
+	public void agregarComponente(Componente componente) { 
+		listaComponentes.add(componente); 
 	}
 
-	public ArrayList<Componente> getComponentes() {
-		return componentes;
-	}
-
-	public void setComponentes(ArrayList<Componente> componentes) {
-		this.componentes = componentes;
-	}
-	
-	public void agregarComponente(Componente componente) {
-		this.componentes.add(componente);
->>>>>>> branch 'main' of https://github.com/UnityIndustries/ProyectoPOOFinal.git
+	public void eliminarComponente(Componente componente) { 
+		listaComponentes.remove(componente); 
 	}
 
 }
