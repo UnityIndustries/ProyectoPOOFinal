61a9b30450429a81a5e0e261ded28dff4df544b0
John Manuel <John Manuel@LAPTOP-4OBTID9R>
2023-07-31 15:45:08 -0400

    arregle algunos errores

--- Proyecto Final POO Grupo 4/src/logico/Factura.java
+++ Proyecto Final POO Grupo 4/src/logico/Factura.java
@@ -3,34 +3,19 @@ package logico;
 import java.util.ArrayList;
 
 public class Factura {
-    private int numeroFactura;
-    private Cliente cliente;
-    private ArrayList<Componente1> componentes;
-    private double total;
+	private int numeroFactura; 
+	private String fechaEmision; 
+	private String nombreCliente; 
+	private ArrayList<Componente> listaComponentes; 
+	private ArrayList<Combo> listaCombos; 
 
-    public Factura(int numeroFactura, Cliente cliente) {
-        this.setNumeroFactura(numeroFactura);
-        this.setCliente(cliente);
-        this.componentes = new ArrayList<>();
-        this.setTotal(0.0);
-    }
-
-    public void agregarComponente(Componente1 componente) {
-        componentes.add(componente);
-        calcularTotal();
-    }
-
-    public void eliminarComponente(Componente1 componente) {
-        componentes.remove(componente);
-        calcularTotal();
-    }
-
-    private void calcularTotal() {
-        setTotal(0.0);
-        for (Componente1 componente : componentes) {
-            setTotal(getTotal() + componente.getPrecio());
-        }
-    }
+	public Factura(int numeroFactura, String fechaEmision, String nombreCliente) {
+		this.numeroFactura = numeroFactura;
+		this.fechaEmision = fechaEmision;
+		this.nombreCliente = nombreCliente;
+		this.listaComponentes = new ArrayList<Componente>();
+		this.listaCombos = new ArrayList<Combo>();
+	}
 
 	public int getNumeroFactura() {
 		return numeroFactura;
@@ -40,21 +25,68 @@ public class Factura {
 		this.numeroFactura = numeroFactura;
 	}
 
-	public Cliente getCliente() {
-		return cliente;
+	public String getFechaEmision() {
+		return fechaEmision;
 	}
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
+	public void setFechaEmision(String fechaEmision) {
+		this.fechaEmision = fechaEmision;
 	}
 
-	public double getTotal() {
-		return total;
+	public String getNombreCliente() {
+		return nombreCliente;
 	}
 
-	public void setTotal(double total) {
-		this.total = total;
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
 	}
 
+	public ArrayList<Componente> getListaComponentes() {
+		return listaComponentes;
+	}
+
+	public void setListaComponentes(ArrayList<Componente> listaComponentes) {
+		this.listaComponentes = listaComponentes;
+	}
+
+	public ArrayList<Combo> getListaCombos() {
+		return listaCombos;
+	}
+
+	public void setListaCombos(ArrayList<Combo> listaCombos) {
+		this.listaCombos = listaCombos;
+	}
+
+	
+	public void agregarComponente(Componente componente) {
+		this.listaComponentes.add(componente);
+	}
+
+	
+	public void eliminarComponente(Componente componente) {
+		this.listaComponentes.remove(componente);
+	}
+
+
+	public void agregarCombo(Combo combo) {
+		this.listaCombos.add(combo);
+	}
+
+
+	public void eliminarCombo(Combo combo) {
+		this.listaCombos.remove(combo);
+	}
+
+
+	public double calcularPrecioTotal() {
+		double precioTotal = 0.0;
+
+		for (Componente componente : listaComponentes) {
+			precioTotal += componente.getPrecio();
+		}
+		for (Combo combo : listaCombos) {
+			precioTotal += combo.getPrecioCombo();
+		}
+		return precioTotal;
+	}
 }
-
